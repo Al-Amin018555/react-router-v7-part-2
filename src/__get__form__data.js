cd02c63abf98991460871e17c269329d86f0ef50
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-19 21:39:10 +0600

    learnt about controlled data collection

--- src/__get__form__data.js
+++ src/__get__form__data.js
@@ -1,4 +1,16 @@
 /** Data collection systems
- * 1. e.target.[name of the input field].value
+ 
+ * 1. e.target.[name of the input field].value (jodi user form submit korar por data collect kore dekhte chai shekhetre ei method use korbo)
+ 
  * 2. use form action and formdata in the action handler. formData.get('name of the input field')
+
+ *3. controlled component. one per each field. use stae on change of the field. useful to dynamically handle error.
+
+ *4. handle all controlled field on one state object
+ const [formData, setFormData] = useState({
+    name: '',
+    password: '',
+    phone: '',
+ })
+
  */
---
+++ src/components/ControlledField/ControlledField.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const ControlledField = () => {
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    const handlePasswordOnChange = (e) => {
+        console.log(e.target.value);
+        setPassword(e.target.value);
+        if (password.length < 6) {
+            setError('password must be of 6 character or more')
+        }
+        else{
+            setError('')
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email" placeholder="Email" required />
+                <br />
+                <input type="password" name="password" onChange={handlePasswordOnChange} defaultValue={password} placeholder="Password" required />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+            <p style={{color:'red'}}><small>{error}</small></p>
+        </div>
+    );
+};
+
+export default ControlledField;
