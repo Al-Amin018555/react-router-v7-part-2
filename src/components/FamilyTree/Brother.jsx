898e3c6c9156b8fb0a92a91eb1a32acdb631c73d
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-21 16:18:13 +0600

    explored more on context api

--- src/components/FamilyTree/Brother.jsx
+++ src/components/FamilyTree/Brother.jsx
@@ -1,7 +1,14 @@
+// import { useContext } from "react";
+import { use } from "react";
+import { MoneyContext } from "./FamilyTree";
+
 const Brother = () => {
+    const [money, setMoney] = use(MoneyContext);
+    // const [money, setMoney] = useContext(MoneyContext); // jekono ekta use korlei hobe
     return (
         <div>
             <h3>Brother</h3>
+            <button onClick={() => setMoney(money + 1000)}>Add 1000 tk</button>
         </div>
     );
 };
--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,20 +1,30 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import './FamilyTree.css'
 import Grandpa from './Grandpa';
 
 export const AssetContext = createContext('');
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
+    const [money, setMoney] = useState(0);
     const asset = 'diamond';
     const newAsset = 'gold';
     return (
         <div className="family-tree">
             <h2>Family Tree</h2>
-            <AssetContext.Provider value={newAsset}>
-                <Grandpa asset={asset}></Grandpa>
-            </AssetContext.Provider>
+            <p>Total Family Money: {money}</p>
+            <MoneyContext value={[money, setMoney]}>
+                <AssetContext.Provider value={newAsset}>
+                    <Grandpa asset={asset}></Grandpa>
+                </AssetContext.Provider>
+            </MoneyContext>
         </div>
     );
 };
 
 export default FamilyTree;
+
+/**
+ * 1. Create a context using createContext with a defalult value
+ * make sure ou export the context to be used in other files
+ */
